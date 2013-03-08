fcb1f69057683c6c665b84af96cac1fd31368a86
WINNERAS <winneras@gmail.com>
2013-03-08 23:28:26 +0000

    add new function for category images

--- winjiejie-concise/partials/posts/post-oneupexp.php
+++ winjiejie-concise/partials/posts/post-oneupexp.php
@@ -9,7 +9,9 @@
 			<p><?php echo get_the_excerpt(); ?> <span class='more-link'><a href="<?php the_permalink(); ?>">Read On&hellip;</a></span></p>
 		</section>
 		<footer class="post-info">
-                        <?php the_post_thumbnail( 'thumbnail' ); ?>
+                        <?php //the_post_thumbnail( 'thumbnail' ); 
+                              frank_the_post_thumbnail()
+                        ?>
 			<?php get_template_part('partials/post-metadata'); ?>
 		</footer>
 	</div>
